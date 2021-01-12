37cbc81da01953defc364f3e0a6698967f61e1df
Nelson Cheung <cubecnelson@gmail.com>
2021-01-13 01:48:04 +0800

    Set up User Auth [ch20]

---
+++ lib/models/user/user_model.dart
@@ -0,0 +1,9 @@
+class UserModel {
+  UserModel(
+      {this.id, this.firstName, this.lastName, this.email, this.phoneNumber});
+  String id;
+  String firstName;
+  String lastName;
+  String email;
+  String phoneNumber;
+}
